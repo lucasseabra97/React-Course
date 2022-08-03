1a9f4ca0945a18655a7f3e94d5d63ec79de7aa5b
lucasseabra <lucasseabra@ua.pt>
2022-08-03 14:55:44 +0100

    setup env

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,8 +1,21 @@
-import './App.css';
+import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [usersList, setUsersList] = useState([])
+
+  const addUserHandle = (uName,uAge) =>{
+    setUsersList((preveUsersList) => {
+      return [...preveUsersList, {name: uName, age: uAge, id: Math.random().toString()}]
+    })
+  }
+
   return (
-    <div className="App">
+    <div>
+      <AddUser onAddUser={addUserHandle}/>
+      <UserList users={usersList}/>
     </div>
   );
 }
