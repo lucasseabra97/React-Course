b0a12d150ed72c2432341537ee3d5fd7fed28e47
lucasseabra <lucasseabra@ua.pt>
2022-08-08 16:29:29 +0100

    feat: added cleanup mechanism

--- 01-side_effect_project/01-starting-project/src/components/Login/Login.js
+++ 01-side_effect_project/01-starting-project/src/components/Login/Login.js
@@ -11,12 +11,23 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+
+    const identifeir = setTimeout(()=>{
+      console.log('Checking form validity! ')
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500)
+
+    return () => {
+      console.log('Cleanup')
+      clearInterval(identifeir)
+    }
+  },[enteredEmail, enteredPassword])
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
