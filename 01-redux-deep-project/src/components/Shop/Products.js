bf6f7f0faa44d6cb8587c7a1cdad4c8533c75c65
lucasseabra97 <lucasseabra@ua.pt>
2024-03-02 20:03:56 +0000

    feat: added reducers, actions and logic implemented

--- 01-redux-deep-project/src/components/Shop/Products.js
+++ 01-redux-deep-project/src/components/Shop/Products.js
@@ -2,15 +2,23 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+  const DUMMY_PRODUCTS = [
+    {id: 'p1', price: 6, title: 'My First Book', description: "The first book I ever wrote"},
+    {id: 'p2', price: 5, title: 'My second Book', description: "The second book I ever wrote"},
+  ]
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
+       {DUMMY_PRODUCTS.map(p => (
         <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+          key={p.id}
+          id={p.id}
+          title={p.title}
+          price={p.price}
+          description={p.description}
         />
+       ))}
       </ul>
     </section>
   );
